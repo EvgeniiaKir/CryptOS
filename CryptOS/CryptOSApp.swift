97fd4b207a1cc863b4ab4040cb7a85f7ba1d34e8
Evgeniia Kiriushina <evgeniya.kiriushina@axonista.com>
2022-07-07 16:48:13 +0200

    Initial Commit

---
+++ CryptOS/CryptOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptOSApp.swift
+//  CryptOS
+//
+//  Created by Евгения Колдышева on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
